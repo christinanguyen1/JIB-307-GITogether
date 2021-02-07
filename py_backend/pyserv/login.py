5affd4b8bc957e1449ec410ac617b770c2760e14
christinanguyen1 <74388673+christinanguyen1@users.noreply.github.com>
2021-02-07 13:40:29 -0500

    Mail configuration

--- py_backend/pyserv/login.py
+++ py_backend/pyserv/login.py
@@ -1,11 +1,20 @@
 # a tiny backend component to get data from the login screen
 
 from flask import Flask, render_template, request
+
+from flask_mail import Mail, Message
 # needs fixing
 # from pydb.pydb_api import check_login_db
 
 app = Flask(__name__)
+app.config['MAIL_SERVER']='smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = 'GITogether307@gmail.com'
+app.config['MAIL_PASSWORD'] = 'gitogether123'
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
 
+mail = Mail(app)
 
 @app.route('/', methods=["GET", "POST"])
 def login():
@@ -13,6 +22,9 @@ def login():
         # get email input
         email= str(request.form['inputEmail'])
         password = str(request.form['inputPassword'])
+        msg = Message('Hello', sender = 'GITogether307@gmail.com', recipients = ['christinan2010@gmail.com'])
+        msg.body = "This is the email body"
+        mail.send(msg)
         return render_template("home.html", email = email, password = password)
     return render_template("index.html")
 
@@ -25,6 +37,19 @@ def signup():
 @app.route('/signout.html', methods=["GET", "POST"])
 def signout():
     return render_template("signout.html")
+
+@app.route('/forgot.html', methods=["GET", "POST"])
+def reset_password():
+    return render_template("forgot.html")
+
+@app.route("/reset.html", methods=["GET", "POST"])
+def index():
+    # recip = str(request.form['inputEmail'])
+    msg = Message('Hello', sender = 'GITogether307@gmail.com', recipients = ['christinan2010@gmail.com'])
+    msg.body = "This is the email body"
+    mail.send(msg)
+    return render_template("reset.html")
+
 
 # loads home page
 # @app.route('/home.html', methods=["GET", "POST"])
