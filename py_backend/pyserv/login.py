45ef55462b5636065a9cd8b3b6435de93a187c73
christinanguyen1 <74388673+christinanguyen1@users.noreply.github.com>
2021-02-07 04:48:10 -0500

    Render home.html, signout.html, and signin.html

--- py_backend/pyserv/login.py
+++ py_backend/pyserv/login.py
@@ -11,14 +11,37 @@ app = Flask(__name__)
 def login():
     if request.method == "POST":
         # get email input
-        email = str(request.form['inputEmail'])
+        email= str(request.form['inputEmail'])
         password = str(request.form['inputPassword'])
-        return "test login: " + email + ":" + password
+        return render_template("home.html", email = email, password = password)
     return render_template("index.html")
 
-# @app.route('/signup.html', methods=["GET", "POST"])
-# def changePage():
-#     return render_template("signup.html")
+# loads signup page
+@app.route('/signup.html', methods=["GET", "POST"])
+def signup():
+    return render_template("signup.html")
+
+# loads signout page
+@app.route('/signout.html', methods=["GET", "POST"])
+def signout():
+    return render_template("signout.html")
+
+# loads home page
+# @app.route('/home.html', methods=["GET", "POST"])
+# def home():
+#     if request.method == "POST":
+#         # get email input
+        
+#         # need to get fname and lname still
+#         email= str(request.form['inputEmail'])
+#         password = str(request.form['inputPassword'])
+#         return render_template("home.html", email = email, password = password,)
+#     return render_template("home.html")
+
+
+
+
+
 
 
 
