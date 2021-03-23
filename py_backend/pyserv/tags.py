cded744d51174b2acddd767fa5106ddce5fbb672
owenns <owens@gatech.edu>
2021-03-23 17:15:25 -0400

    add tags framework

---
+++ py_backend/pyserv/tags.py
@@ -0,0 +1,45 @@
+import sqlite3
+imprt
+
+# tag error classes
+
+
+class TagError:
+    pass
+
+
+class ClubDoesNotExist(TagError):
+    pass
+
+
+class MalformedTag(TagError):
+    pass
+
+# TAG-based search API
+
+
+class TagMachine:
+
+    def __init__(self, database):
+        self.conn = sqlite3.connect(database)
+        self.db = self.conn.cursor()
+
+    def get_club_tags(club_name, tags):
+        for tag in tags:
+            if (' ' in tag):
+                raise MalformedTag
+            else:
+
+        pass
+
+    def add_club_tags():
+        pass
+
+    def clear_club_tags():
+        pass
+
+    def remove_club_tags():
+        pass
+
+    def search_club_by_tags():
+        pass
