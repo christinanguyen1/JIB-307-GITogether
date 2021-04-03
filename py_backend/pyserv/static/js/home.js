1733b4421e6d681801a61f97beb205c319cb1656
christinanguyen1 <74388673+christinanguyen1@users.noreply.github.com>
2021-04-03 19:44:07 -0400

    Move club_page.js

---
+++ py_backend/pyserv/static/js/home.js
@@ -0,0 +1,7 @@
+var search_str = document.getElementById("search");
+
+// get search string
+var search_button = document.getElementById("search_button");
+search_button.addEventListener("click", function() {
+    console.log(search_str.value);
+} );
